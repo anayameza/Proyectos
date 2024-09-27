bc930fa146c2440bf6e4164d2423843ea3535cc5
anayameza <107013758+anayameza@users.noreply.github.com>
2024-09-27 10:54:15 -0500

    Delete php directory

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
-
-    if($conexion){
-        echo 'Conectado exitosamente a la base de datos';
-    }else{
-        echo 'no se conecto a la base de datos';
-    }
-?>
